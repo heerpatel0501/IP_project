5234596f05e3686d573628c1f7c5fc8995e71d3d
Heer <heerpatel0501@gmail.com>
2025-12-10 18:58:36 +0530

    first commit

---
+++ thirdpage.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+
+    let fullName = prompt("Enter your Full Name without space :");
+
+    let usernameCorrect = fullName + '@' + fullName.length ; 
+
+    document.getElementById("username").value = usernameCorrect;
+
+    alert("Set Password = HR0507");
+
+    window.usernameCorrect =usernameCorrect; 
+
+}
+
+
+function login(){
+    let userInput = document.getElementById("username").value;
+    let passInput = document.getElementById("password").value;
+
+    let passwordCorrect = "HR0507" ; 
+
+    if (userInput === window.usernameCorrect && passInput === passwordCorrect ){
+        alert("👍 Login Succesful!");
+        window.location.href="mainpage.html";
+    }
+    else{
+        alert("❌ Something went wrong. Try again! ");
+    }
+
+}
